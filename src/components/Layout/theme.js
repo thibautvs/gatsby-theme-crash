47e197f6ca87a5976782144d729e95dc2ad6df76
Thibaut Van Spaandonck <thibaut@urge2code.com>
2018-09-24 12:33:51 +0200

    Added ThisWorksInProd component

--- src/components/Layout/theme.js
+++ src/components/Layout/theme.js
@@ -1,6 +1,7 @@
 const theme = {
   colors: {
     green: '#31c2bc',
+    gray: '#888'
   }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 import Layout from '../components/Layout'
 import WorkWithUs from './index/WorkWithUs'
+
+const ThisWorksInProd = styled.h1`
+  color: ${props => props.theme.colors.gray};
+`
 
 const IndexPage = () => (
   <Layout>
     <section>
+      <ThisWorksInProd>Gatsby Theme Crash</ThisWorksInProd>
       <p>
         This works in dev (gatsby develop), but not when building for prod
         (gatsby build)
